f62a351d249671ac3c34f177598ceaeb6737bf4d
Boshen Hu <huboshen@hotmail.com>
2020-11-13 02:03:02 +1100

    complete game over condition and show game over panel

--- src/components/FlappyBird.vue
+++ src/components/FlappyBird.vue
@@ -5,6 +5,10 @@
             <pipe v-for="(pipe, i) in pipes" :key="i" :pipe-style="pipe"></pipe>
         </div>
         <div id="ground"></div>
+        <game-over-panel
+            v-show="showGameOverPanel"
+            :score="score"
+        ></game-over-panel>
     </div>
 </template>
 
@@ -30,18 +34,25 @@ const SETTINGS = {
 };
 import Pipe from "@/components/Pipe";
 import Bird from "@/components/Bird";
+import GameOverPanel from "@/components/GameOverPanel";
 export default {
     name: "FlappyBird",
     components: {
         Pipe,
         Bird,
+        GameOverPanel,
     },
     data() {
         return {
-            bird: { bottom: 200, left: 50 },
+            bird: { bottom: 200, left: 80 },
             pipes: [],
             pipeMover: null,
             birdMover: null,
+            birdMoveRID: null,
+            pipeMoveRID: null,
+            isGaming: false,
+            score: 0,
+            showGameOverPanel: false,
         };
     },
     props: {},
@@ -65,70 +76,174 @@ export default {
                 right: -PIPE_DIMENTIONS.width,
                 pipeTopTop: -PIPE_DIMENTIONS.height + 30 + currentExtendOffset,
                 pipeBottomTop: fixEdge + currentExtendOffset + currentGap,
+                status: "inComing",
             });
         },
         movePipe() {
+            const vm = this;
             const backgroundWidth = this.$refs.background.offsetWidth;
-            this.pipeMover = setInterval(() => {
-                this.pipes.forEach((pipe) => {
+
+            function animate({ timing, draw }) {
+                let start = performance.now();
+
+                vm.pipeMoveRID = requestAnimationFrame(function animate(time) {
+                    // timeElapsed goes from 0 to 1
+                    let timeElapsed = (time - start) / 1000; // seconds
+
+                    // calculate the current animation state
+                    let progress = timing(timeElapsed);
+
+                    draw(progress); // draw it
+
+                    vm.pipeMoveRID = requestAnimationFrame(animate);
+
+                    // check game over condition
+                    vm.gameStatusHandler();
+                });
+            }
+            function timing(timeElapsed) {
+                return 1;
+            }
+            function draw(progress) {
+                vm.pipes.forEach((pipe) => {
                     // remove pipe if is outside of the game left border
                     if (pipe.right >= backgroundWidth) {
-                        this.pipes.shift();
+                        vm.pipes.shift();
                     }
 
                     if (
-                        this.pipes[this.pipes.length - 1].right >
+                        vm.pipes[vm.pipes.length - 1].right >
                         SETTINGS.pipeInterval
                     ) {
-                        this.generatePipe();
+                        vm.generatePipe();
                     }
-                    pipe.right += 1;
+                    pipe.right += progress;
                 });
-            }, 20);
+            }
+            animate({ timing, draw });
         },
-        moveBird() {},
+        moveBird() {
+            document.addEventListener(
+                "keydown",
+                this.spaceEventListener(),
+                false
+            );
+        },
+        spaceEventListener() {
+            const vm = this;
+            // https://javascript.info/js-animation#:~:text=Using%20setInterval,second%2C%20then%20it%20looks%20smooth.
+            function animate({ timing, draw }) {
+                let start = performance.now();
+
+                vm.birdMoveRID = requestAnimationFrame(function animate(time) {
+                    // timeElapsed goes from 0 to 1
+                    let timeElapsed = (time - start) / 1000; // seconds
+
+                    // calculate the current animation state
+                    let progress = timing(timeElapsed);
+
+                    draw(progress); // draw it
+
+                    if (vm.bird.bottom >= 0) {
+                        vm.birdMoveRID = requestAnimationFrame(animate);
+                    } else {
+                        vm.bird.bottom = 0;
+                    }
+                });
+            }
+            function timing(timeElapsed) {
+                return -40 * Math.pow(timeElapsed - 0.12, 2) + 2.1;
+            }
+
+            function draw(progress) {
+                vm.bird.bottom += progress;
+            }
+
+            return ({ keyCode }) => {
+                // space
+                if (keyCode === 32) {
+                    if (vm.isGaming) {
+                        // clicking space for bird jumping
+                        if (vm.birdMoveRID)
+                            cancelAnimationFrame(vm.birdMoveRID);
+                        animate({ timing, draw });
+                    }
+                }
+
+                // enter
+                if (!vm.isGaming && keyCode === 13) {
+                    // start/restart the game
+                    vm.resetGameData();
+                    vm.isGaming = true;
+                    this.generatePipe();
+                    this.movePipe();
+                }
+
+                if (keyCode === 83) {
+                    if (vm.pipeMoveRID) cancelAnimationFrame(vm.pipeMoveRID);
+                    if (vm.birdMoveRID) cancelAnimationFrame(vm.birdMoveRID);
+                }
+            };
+        },
+        gameStatusHandler() {
+            const nearestPipe = this.pipes.find(
+                (pipe) => pipe.status !== "passed"
+            ); // find the latest in coming or passing pipe
+            this.birdPipeStatus(nearestPipe); // update status for the
+            switch (nearestPipe.status) {
+                case "passing":
+                    const gapTop =
+                        PIPE_DIMENTIONS.height + nearestPipe.pipeTopTop;
+                    const gapBottomTop = nearestPipe.pipeBottomTop;
+                    if (
+                        this.bird.bottom + BIRD_DIMENTIONS.height + gapTop >=
+                            PLAYGROUND_DIMENTIONS.height ||
+                        this.bird.bottom + gapBottomTop <=
+                            PLAYGROUND_DIMENTIONS.height
+                    ) {
+                        this.handleGameOver();
+                    }
+                    break;
+            }
+        },
+        birdPipeStatus(nearestPipe) {
+            const oldStatus = nearestPipe.status;
+            const pipeRightEdgeLeftosition =
+                PLAYGROUND_DIMENTIONS.width - nearestPipe.right;
+            const pipeLeftEdgeLeftosition =
+                pipeRightEdgeLeftosition - PIPE_DIMENTIONS.width;
+            const birdRightEdgeLeftPosition =
+                this.bird.left + BIRD_DIMENTIONS.width;
+
+            let status = "passing";
+            if (pipeLeftEdgeLeftosition - birdRightEdgeLeftPosition >= 0) {
+                status = "inComing";
+            } else if (pipeRightEdgeLeftosition - this.bird.left <= 0) {
+                status = "passed";
+                if (oldStatus === "passing") this.score += 1; // passing => passed, gain one score
+            }
+            nearestPipe.status = status;
+        },
+        handleGameOver() {
+            this.isGaming = false;
+            if (this.pipeMoveRID) cancelAnimationFrame(this.pipeMoveRID);
+            if (this.birdMoveRID) cancelAnimationFrame(this.birdMoveRID);
+            this.showGameOverPanel = true;
+        },
+        resetGameData() {
+            this.bird = { bottom: 200, left: 80 };
+            this.pipes = [];
+            this.pipeMover = null;
+            this.birdMover = null;
+            this.birdMoveRID = null;
+            this.pipeMoveRID = null;
+            this.isGaming = false;
+            this.score = 0;
+            this.showGameOverPanel = false;
+        },
     },
     mounted() {
-        const vm = this;
-        document.onkeydown = ({ keyCode }) => {
-            if (keyCode == 32) {
-                // https://javascript.info/js-animation#:~:text=Using%20setInterval,second%2C%20then%20it%20looks%20smooth.
-                function animate({ timing, draw }) {
-                    let start = performance.now();
-
-                    vm.requestID = requestAnimationFrame(function animate(
-                        time
-                    ) {
-                        // timeElapsed goes from 0 to 1
-                        let timeElapsed = (time - start) / 1000; // seconds
-
-                        // calculate the current animation state
-                        let progress = timing(timeElapsed);
-
-                        draw(progress); // draw it
-
-                        if (vm.bird.bottom >= 0) {
-                            vm.requestID = requestAnimationFrame(animate);
-                        } else {
-                            vm.bird.bottom = 0;
-                        }
-                    });
-                }
-
-                function back(timeElapsed) {
-                    return -40 * Math.pow(timeElapsed - 0.12, 2) + 2.1;
-                }
-
-                function draw(progress) {
-                    vm.bird.bottom += progress;
-                }
-
-                if (vm.requestID) cancelAnimationFrame(vm.requestID);
-                animate({ timing: back, draw });
-            }
-        };
-        this.generatePipe();
-        // this.movePipe();
+        this.moveBird();
     },
 };
 </script>
---
+++ src/components/GameOverPanel.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="game-over-panel">
+        <div id="game-over-title"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "GameOverPanel",
+    props: {
+        score: {
+            type: Number,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style></style>
